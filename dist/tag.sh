f479b582bdd3b69e86f0b0ec5d3f85a5bc9ef81f
j.bakker <j.bakker@atmind.nl>
2009-11-30 17:59:35 +0000

    python31

--- dist/tag.sh
+++ dist/tag.sh
@@ -1,1 +1,2 @@
-svn copy https://blender-aid.googlecode.com/svn/trunk/ https://blender-aid.googlecode.com/svn/tags/20091022
+export version=$(date +"%Y%m%d")
+svn copy https://blender-aid.googlecode.com/svn/trunk/ https://blender-aid.googlecode.com/svn/tags/$version
--- src/settings.py
+++ src/settings.py
@@ -32,7 +32,7 @@ SQLITE3_CONNECTIONURL="/var/tmp/sql.bin"
 STYLE_SHEET="site.css"
 
 #release version of the system
-VERSION="20091126"
+VERSION="20091130"
 
 #additional description of the release
 VERSION_DESCRIPTION="November 2009"
