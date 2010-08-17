01fde68285d00bba6fa5a69a90014707e499268a
j.bakker@atmind.nl <j.bakker@atmind.nl@de202430-a8d8-11de-8ba5-91705c1537ed>
2010-08-17 18:48:30 +0000

    svn unittests

--- trunk/src/testsvn.py
+++ trunk/src/testsvn.py
@@ -1,13 +1,36 @@
+import unittest
 import pysvn
+
+class TestCase(unittest.TestCase):
+    pass
 
 def testLogin( realm, username, may_save ):
     return True, "jbakker", "zx098zx", False
 
 def notify( event ):
-    print(event)
+    print(event["action"], event["path"])
     return
 
-client = pysvn.Client()
-client.callback_notify = notify
-client.callback_get_login = testLogin
-client.checkout("http://atmind/svn/yofrankie", "/home/dev/yofrankie");
+class ScenarioBasicSVN(TestCase):
+
+    def testSvnCheckout(self):
+        client = pysvn.Client()
+        client.callback_notify = notify
+        client.callback_get_login = testLogin
+        client.checkout("http://atmind/svn/yofrankie", "/tmp/yofrankie");
+    
+    def testUpdate(self):
+        client = pysvn.Client()
+        client.callback_notify = notify
+        client.callback_get_login = testLogin
+        client.update("/tmp/yofrankie");
+
+    def testLog(self):
+        client = pysvn.Client()
+        client.callback_notify = notify
+        client.callback_get_login = testLogin
+        logs = client.log("/tmp/yofrankie");
+        print(logs)
+        
+if __name__ =='__main__':
+    unittest.main()
