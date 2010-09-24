600ec3d9b56491504f064e61ec1119f56d946c4d
m.dewanchand <m.dewanchand@atmind.nl>
2010-09-24 14:13:00 +0000

    style svn tab

--- src/server.py
+++ src/server.py
@@ -17,7 +17,7 @@
 #
 # ***** END GPL LICENCE BLOCK *****
 #
-# (c) 2009, At Mind B.V. - Jeroen Bakker
+# (c) 2009, At Mind B.V. - Jeroen Bakker, M.Dewanchand
 
 
 ######################################################
